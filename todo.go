23fa6d65e546810142d8763afaf54564ec90d887
bintangcloud <bintangkelaci@gmail.com>
2026-07-14 22:49:01 +0800

    membuat struct

---
+++ todo.go
@@ -0,0 +1,17 @@
+package main
+
+type User struct {
+	ID    uint   `gorm:"primaryKey" json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+
+	Todos []Todo `json:"todos"`
+}
+
+type Todo struct {
+	ID          uint   `gorm:"primaryKey" json:"id"`
+	Title       string `json: "title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	UserID      uint   `json:"user_id"`
+}
