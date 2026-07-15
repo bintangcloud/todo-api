d6ceecb2c1db5dfd5363cf506f526a45597b588a
bintangcloud <bintangkelaci@gmail.com>
2026-07-15 20:55:45 +0800

    menambah endpoint update data user

--- main.go
+++ main.go
@@ -33,4 +33,30 @@ func main() {
 		c.JSON(200, gin.H{"status": "User berhasil ditambahkan"})
 	})
 
+	r.GET("/tampilkan-user", func(c *gin.Context) {
+		var AllUsers []User
+
+		db.Find(&AllUsers)
+		c.JSON(200, gin.H{"data": AllUsers})
+	})
+
+	r.PUT("/update-user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var UserLama User
+
+		if err := db.First(&UserLama, id).Error; err != nil {
+			c.JSON(404, gin.H{"error": "User tidak ditemukan!"})
+			return
+		}
+
+		var UserBaru User
+		if err := c.ShouldBindJSON(&UserBaru); err != nil {
+			c.JSON(400, gin.H{"error": "Format JSON salah"})
+			return
+		}
+
+		db.Model(&UserLama).Updates(UserBaru)
+		c.JSON(200, gin.H{"status": "User berhasil diupdate!"})
+	})
+
 }
