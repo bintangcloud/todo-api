197211add24eed45d43a2f9026abf5df6c623201
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 23:03:33 +0800

    membuat handler untuk membuat todo

---
+++ controllers/todo_controller.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"todo-api/database"
+	"todo-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTodos(c *gin.Context) {
+	var TodoBaru models.Todo
+	if err := c.ShouldBindJSON(&TodoBaru); err != nil {
+		c.JSON(400, gin.H{"error": "Format JSON salah"})
+		return
+	}
+	database.DB.Create(&TodoBaru)
+	c.JSON(200, gin.H{"status": "Todo berhasil ditambahkan"})
+}
