5f46e2297917d21d9e7a84a950542d7e34b48bb5
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 22:41:50 +0800

    memindahkan dan membuat handler untuk create user

---
+++ controllers/user_controller.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUsers(c *gin.Context) {
+	r.POST("/users", func(c *gin.Context) {
+		var UserBaru User
+		if err := c.ShouldBindJSON(&UserBaru); err != nil {
+			c.JSON(400, gin.H{"error": "Format JSON salah"})
+			return
+		}
+
+		db.Create(&UserBaru)
+		c.JSON(200, gin.H{"status": "User berhasil ditambahkan"})
+	})
+}
