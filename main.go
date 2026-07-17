16119666818ed1ee91375c31574660d3fd8253fd
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 22:32:55 +0800

    memindahkan struct of user ke package models

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "user=postgres password=bintang444 dbname=todo_db port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Gagal membuka koneksi GORM:", err)
-	}
-	fmt.Println("Koneksi GORM berhasil")
-
-	db.AutoMigrate(&User{}, &Todo{})
-	fmt.Println("Migrasi tabel berhasil")
 
 	r := gin.Default()
 
