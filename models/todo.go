9a6817b48efe869f7cc33f750c003a3675e6c7e9
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 22:33:33 +0800

    memindahkan struct of todo ke models

--- models/todo.go
+++ models/todo.go
@@ -1,9 +1,9 @@
 package models
 
-type User struct {
-	ID    uint   `gorm:"primaryKey" json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-
-	Todos []Todo `json:"todos"`
+type Todo struct {
+	ID          uint   `gorm:"primaryKey" json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	UserID      uint   `json:"user_id"`
 }
