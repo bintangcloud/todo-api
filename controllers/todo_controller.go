3c01a8adfab5af2f1a418e551106243e97a5849d
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 23:06:33 +0800

    menambahkan handler untuk mengupdate todos

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -23,3 +23,22 @@ func GetAllTodos(c *gin.Context) {
 	database.DB.Find(&AllTodos)
 	c.JSON(200, gin.H{"data": AllTodos})
 }
+
+func UpdateTodos(c *gin.Context) {
+	id := c.Param("id")
+	var TodoLama models.Todo
+
+	if err := database.DB.First(&TodoLama, id).Error; err != nil {
+		c.JSON(404, gin.H{"error": "Todo tidak ditemukan!"})
+		return
+	}
+
+	var TodoBaru models.Todo
+	if err := c.ShouldBindJSON(&TodoBaru); err != nil {
+		c.JSON(400, gin.H{"error": "Format JSON salah"})
+		return
+	}
+
+	database.DB.Model(&TodoLama).Updates(TodoBaru)
+	c.JSON(200, gin.H{"status": "Todo berhasil diupdate!"})
+}
