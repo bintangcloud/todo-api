b232603d7366162ac6c46ecb2304edaa7c465bfb
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 23:13:02 +0800

    Membuat routes

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"todo-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	r.POST("/users", controllers.CreateUsers)
+	r.GET("/users", controllers.GetAllUsers)
+	r.PUT("/users/:id", controllers.UpdateUser)
+	r.DELETE("/users/:id", controllers.DeleteUser)
+
+	r.POST("/todos", controllers.CreateTodos)
+	r.GET("/todos", controllers.GetAllTodos)
+	r.PUT("/todos/:id", controllers.UpdateTodos)
+	r.DELETE("/todos/:id", controllers.DeleteTodos)
+}
