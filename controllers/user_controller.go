a0bdaaa9c00f543f55ade476435438a3c3a316b4
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 22:52:16 +0800

    memperbaiki handler, menghapus kesalahan pengisian route yang salah folder

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,59 +1,52 @@
 package controllers
 
 import (
+	"todo-api/database"
 	"todo-api/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUsers(c *gin.Context) {
-	r.POST("/users", func(c *gin.Context) {
-		var UserBaru models.User
-		if err := c.ShouldBindJSON(&UserBaru); err != nil {
-			c.JSON(400, gin.H{"error": "Format JSON salah"})
-			return
-		}
+	var UserBaru models.User
+	if err := c.ShouldBindJSON(&UserBaru); err != nil {
+		c.JSON(400, gin.H{"error": "Format JSON salah"})
+		return
+	}
 
-		db.Create(&UserBaru)
-		c.JSON(200, gin.H{"status": "User berhasil ditambahkan"})
-	})
+	database.DB.Create(&UserBaru)
+	c.JSON(200, gin.H{"status": "User berhasil ditambahkan"})
 }
 
 func GetAllUsers(c *gin.Context) {
-	r.GET("/users", func(c *gin.Context) {
-		var AllUsers []models.User
+	var AllUsers []models.User
 
-		db.Find(&AllUsers)
-		c.JSON(200, gin.H{"data": AllUsers})
-	})
+	database.DB.Find(&AllUsers)
+	c.JSON(200, gin.H{"data": AllUsers})
 }
 
 func UpdateUser(c *gin.Context) {
-	r.PUT("/users/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		var UserLama models.User
+	id := c.Param("id")
+	var UserLama models.User
 
-		if err := db.First(&UserLama, id).Error; err != nil {
-			c.JSON(404, gin.H{"error": "User tidak ditemukan!"})
-			return
-		}
+	if err := database.DB.First(&UserLama, id).Error; err != nil {
+		c.JSON(404, gin.H{"error": "User tidak ditemukan!"})
+		return
+	}
 
-		var UserBaru models.User
-		if err := c.ShouldBindJSON(&UserBaru); err != nil {
-			c.JSON(400, gin.H{"error": "Format JSON salah"})
-			return
-		}
+	var UserBaru models.User
+	if err := c.ShouldBindJSON(&UserBaru); err != nil {
+		c.JSON(400, gin.H{"error": "Format JSON salah"})
+		return
+	}
 
-		db.Model(&UserLama).Updates(UserBaru)
-		c.JSON(200, gin.H{"status": "User berhasil diupdate!"})
-	})
+	database.DB.Model(&UserLama).Updates(UserBaru)
+	c.JSON(200, gin.H{"status": "User berhasil diupdate!"})
 }
 
 func DeleteUser(c *gin.Context) {
-	r.DELETE("/users/:id", func(c *gin.Context) {
-		id := c.Param("id")
+	id := c.Param("id")
 
-		db.Delete(&models.User{}, id)
-		c.JSON(200, gin.H{"status": "sukses menghapus user id" + id})
-	})
+	database.DB.Delete(&models.User{}, id)
+	c.JSON(200, gin.H{"status": "sukses menghapus user id" + id})
 }
