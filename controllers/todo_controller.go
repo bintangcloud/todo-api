5d2c644acd1a7a89eeebccd64a27c84ac006e873
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 23:05:02 +0800

    menambahkan handler untuk menampilkan semua todo

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -16,3 +16,10 @@ func CreateTodos(c *gin.Context) {
 	database.DB.Create(&TodoBaru)
 	c.JSON(200, gin.H{"status": "Todo berhasil ditambahkan"})
 }
+
+func GetAllTodos(c *gin.Context) {
+	var AllTodos []models.Todo
+
+	database.DB.Find(&AllTodos)
+	c.JSON(200, gin.H{"data": AllTodos})
+}
