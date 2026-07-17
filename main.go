3e9f74085ca5a272a1c36ab87a8d054a277fe808
bintangcloud <bintangkelaci@gmail.com>
2026-07-17 23:15:02 +0800

    Memperbaharui isi main

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"todo-api/database"
+	"todo-api/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	database.ConnectDB()
+
 	r := gin.Default()
+
+	routes.SetupRoutes(r)
 
 	r.Run(":8080")
 }
