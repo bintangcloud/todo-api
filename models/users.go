368a46bbc0f94e7bbbf33c752e23d3cafc0cafb5
bintangcloud <bintangkelaci@gmail.com>
2026-07-18 23:14:30 +0800

    menambahkan password pada model users

--- models/users.go
+++ models/users.go
@@ -4,6 +4,7 @@ type User struct {
 	ID    uint   `gorm:"primaryKey" json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+	Password string `json:"password"`
 
 	Todos []Todo `json:"todos"`
 }
