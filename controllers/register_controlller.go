52440262ab92892842ccfc97acccb2f53ac37f92
bintangcloud <bintangkelaci@gmail.com>
2026-07-18 23:35:18 +0800

    import hashing password ke func register

--- controllers/register_controlller.go
+++ controllers/register_controlller.go
@@ -3,16 +3,31 @@ package controllers
 import (
 	"todo-api/database"
 	"todo-api/models"
+	"todo-api/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterUser(c *gin.Context) {
-	var UserRegister models.User
-	if err := c.ShouldBindJSON(&UserRegister); err != nil {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(400, gin.H{"error": "Format JSON salah"})
 		return
 	}
-	database.DB.Create(&UserRegister)
-	c.JSON(200, gin.H{"status": "Register Berhasil"})
+
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Gagal mengenkripsi password"})
+		return
+	}
+
+	user.Password = hashedPassword
+
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(500, gin.H{
+			"error": "Gagal menyimpan data",
+		})
+		return
+	}
+	c.JSON(201, gin.H{"status": "Register Berhasil"})
 }
