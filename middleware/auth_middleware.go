48bcc6ecc3f0909642bf21e7ba9308b8693be765
bintangcloud <bintangkelaci@gmail.com>
2026-08-03 15:15:16 +0800

    merampungkan middleware

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"strings"
+	"todo-api/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+
+	if authHeader == "" {
+		c.JSON(401, gin.H{
+			"error": "Authorization Header Dibutuhkan",
+		})
+		c.Abort()
+		return
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return utils.SecretKey, nil
+	})
+
+	if err != nil || !token.Valid {
+		c.JSON(401, gin.H{
+			"error": "Token tidak valid",
+		})
+		c.Abort()
+		return
+	}
+
+}
