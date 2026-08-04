3eb3503fcb810af2247ac77a61c7f676570c0e18
bintangcloud <bintangkelaci@gmail.com>
2026-08-04 22:57:55 +0800

    Memperbaharui routes dengan grouping authorize

--- routes/routes.go
+++ routes/routes.go
@@ -12,12 +12,12 @@ func SetupRoutes(r *gin.Engine) {
 
 	r.POST("/login", controllers.Login)
 
-	r.GET("/users", controllers.GetAllUsers)
-	r.PUT("/users/:id", controllers.UpdateUser)
-	r.DELETE("/users/:id", controllers.DeleteUser)
-
 	authorized := r.Group("/")
 	authorized.Use(middleware.AuthMiddleware())
+	authorized.GET("/users", controllers.GetAllUsers)
+	authorized.PUT("/users/:id", controllers.UpdateUser)
+	authorized.DELETE("/users/:id", controllers.DeleteUser)
+
 	authorized.POST("/todos", controllers.CreateTodos)
 	authorized.GET("/todos", controllers.GetAllTodos)
 	authorized.PUT("/todos/:id", controllers.UpdateTodos)
