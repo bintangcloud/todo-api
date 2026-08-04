bbebeec8fa2b46b0ab741ea46fb3718e7405b6d2
bintangcloud <bintangkelaci@gmail.com>
2026-08-04 23:30:38 +0800

    coba

--- database/db.go
+++ database/db.go
@@ -3,24 +3,39 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 	"todo-api/models"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
 func ConnectDB() {
 
-	dsn := "user=postgres password=bintang444 dbname=todo_db port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	dsn := fmt.Sprintf(
+		"user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Gagal membuka koneksi GORM:", err)
 	}
+
 	fmt.Println("Koneksi GORM berhasil")
 
 	db.AutoMigrate(&models.User{}, &models.Todo{})
+
 	fmt.Println("Migrasi tabel berhasil")
 
 	DB = db
