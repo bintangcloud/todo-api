8b5b637d5d4ec48cbe0032836225ecd19bd15792
bintangcloud <bintangkelaci@gmail.com>
2026-08-07 23:16:18 +0800

    Merevisi validasi pada struct todo

--- models/todo.go
+++ models/todo.go
@@ -3,7 +3,7 @@ package models
 type Todo struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
 	Title       string `json:"title" binding:"required"`
-	Description string `json:"description" binding:"required"`
-	Status      string `json:"status" binding:"required"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
 	UserID      uint   `json:"user_id"`
 }
