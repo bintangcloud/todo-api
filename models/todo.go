5099b83bc66693d88ea6a8b156a68fc2e782938c
bintangcloud <bintangkelaci@gmail.com>
2026-08-07 23:14:38 +0800

    Menambahkan validation di struct todo

--- models/todo.go
+++ models/todo.go
@@ -2,8 +2,8 @@ package models
 
 type Todo struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Status      string `json:"status" binding:"required"`
 	UserID      uint   `json:"user_id"`
 }
